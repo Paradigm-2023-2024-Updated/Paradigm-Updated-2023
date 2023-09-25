23177b0f6ca2cc5d098471610de23430c651883e
ahsj5 <ahsj5@schls.albco>
2023-09-25 16:47:42 -0400

    Color sensor code draft

---
+++ FtcRobotController-9.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColSensor.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+
+@TeleOp(name = "ColSensor")
+public class ColSensor extends OpMode {
+    //establishing color sensor variable
+    ColorSensor sensorColor;
+    @Override
+    public void init(){
+        //Connect color sensor variable to hardware
+        sensorColor = hardwareMap.get(ColorSensor.class, "Color");
+
+    }
+
+    @Override
+    public void loop(){
+
+        //Display color values on phone
+        telemetry.addData("Red", sensorColor.red());
+        telemetry.addData("Green", sensorColor.green());
+        telemetry.addData("Blue", sensorColor.green());
+        telemetry.update();
+    }
+
+}
