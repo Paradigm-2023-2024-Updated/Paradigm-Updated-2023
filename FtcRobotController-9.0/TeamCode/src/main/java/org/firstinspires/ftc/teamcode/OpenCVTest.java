da3e40f940cb3f92cb35e7a8877ead5e96be6eae
SkinnyPenguin <112586940+SkinnyPenguin@users.noreply.github.com>
2023-10-16 17:08:02 -0400

    .

--- FtcRobotController-9.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCVTest.java
+++ FtcRobotController-9.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCVTest.java
@@ -1,15 +1,15 @@
 
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 import org.openftc.easyopencv.OpenCvWebcam;
 
-
+@TeleOp(name = "OpenCVTest")
 public class OpenCVTest extends OpMode {
 
     OpenCvWebcam webcam1 = null;
@@ -17,10 +17,10 @@ public class OpenCVTest extends OpMode {
     @Override
     public void init() {
 
-        WebcamName webcamName = hardwareMap.get(WebcamName.class,"webcam");
+        WebcamName webcamName = hardwareMap.get(WebcamName.class,"Webcam");
         AprilTagProcessor aprilTagProcessor = AprilTagProcessor.easyCreateWithDefaults();
         VisionPortal visionPortal = VisionPortal.easyCreateWithDefaults(webcamName , aprilTagProcessor);
-        //int cameraMonitorViewId  = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId","webCamName","webCamName");
+        int cameraMonitorViewId  = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId","webCamName","webCamName");
 
 
 
