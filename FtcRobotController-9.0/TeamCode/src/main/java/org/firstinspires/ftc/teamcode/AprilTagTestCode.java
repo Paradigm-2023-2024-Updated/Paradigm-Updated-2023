23e68edf0cc7d6d73f0dcc64cfaf973cd8f50f4b
Bkpy0 <bkpy0@k12albemarle.org>
2023-10-25 16:24:39 -0400

    Fixing localization error for variable tagProcessor in April Tag code

--- FtcRobotController-9.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagTestCode.java
+++ FtcRobotController-9.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagTestCode.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import android.util.Size;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
@@ -11,48 +12,41 @@ import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 
 @TeleOp(name = "AprilTagTestCode")
-public class AprilTagTestCode extends OpMode {
-
-
-
-    AprilTagProcessor tagProcessor = new AprilTagProcessor.Builder()
-            .setDrawAxes(true)
-            .setDrawCubeProjection(true)
-            .setDrawTagID(true)
-            .build();
-
-    VisionPortal visionPortal = new VisionPortal.Builder()
-            .addProcessor(tagProcessor)
-            .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
-            .setCameraResolution(new Size(640,480)) //adjust width and height as needed
-            .build();
+public class AprilTagTestCode extends LinearOpMode {
 
     @Override
-    public void init(){
-   // public void runOpMode() throws InterruptedException{
+    public void runOpMod() throws InterruptedException {
+
+        AprilTagProcessor tagProcessor = new AprilTagProcessor.Builder()
+                .setDrawAxes(true)
+                .setDrawCubeProjection(true)
+                .setDrawTagID(true)
+                .build();
+
+        VisionPortal visionPortal = new VisionPortal.Builder()
+                .addProcessor(tagProcessor)
+                .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
+                .setCameraResolution(new Size(640, 480)) //adjust width and height as needed
+                .build();
 
 
+        waitForStart();
 
-    }
+        while (!isStopRequested() && opModeIsActive()) {
 
-    @Override
-    public void loop(){
-   // waitForStart();
+            if (tagProcessor.getDetections().size() > 0) {
+                AprilTagDetection tag = tagProcessor.getDetections().get(0);
 
-    //while (!isStopRequested() && opModeIsActive()){
+                telemetry.addData("x", tag.ftcPose.x);
+                telemetry.addData("y", tag.ftcPose.y);
+                telemetry.addData("z", tag.ftcPose.z);
+                telemetry.addData("roll", tag.ftcPose.roll);
+                telemetry.addData("pitch", tag.ftcPose.pitch);
+                telemetry.addData("yaw", tag.ftcPose.yaw);
+            }
 
-        if (tagProcessor.getDetections().size()>0){
-            AprilTagDetection tag = tagProcessor.getDetections().get(0);
-
-            telemetry.addData("x",tag.ftcPose.x);
-            telemetry.addData("y",tag.ftcPose.y);
-            telemetry.addData("z",tag.ftcPose.z);
-            telemetry.addData("roll",tag.ftcPose.roll);
-            telemetry.addData("pitch",tag.ftcPose.pitch);
-            telemetry.addData("yaw",tag.ftcPose.yaw);
+            telemetry.update();
         }
-
-        telemetry.update();
     }
 
 
